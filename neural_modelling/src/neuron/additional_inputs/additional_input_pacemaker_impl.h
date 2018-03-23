3567508d7b42a5e2f44954c024eae4707184588b
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2018-03-23 15:46:45 +0000

    Add fulle exp and divide version

--- neural_modelling/src/neuron/additional_inputs/additional_input_pacemaker_impl.h
+++ neural_modelling/src/neuron/additional_inputs/additional_input_pacemaker_impl.h
@@ -23,39 +23,12 @@ static input_t additional_input_get_input_value_as_current(
         additional_input_pointer_t additional_input,
         state_t membrane_voltage) {
 
-	REAL m_inf_V_shift = (membrane_voltage >> 7) + 0.7421875k;
-	REAL tau_m_V_shift = (membrane_voltage >> 7) - 0.234375k;
-//	log_info("m_V: %k, tau_m: %k", m_inf_V_shift, tau_m_V_shift);
+	additional_input->m_inf = 1 / (1 + expk((membrane_voltage+75)/5.5));
 
-	// **** Update pacemeaker current ****
-	if (membrane_voltage <= -138k){
-		additional_input->m_inf = 1.0k;
-
-	} else if (membrane_voltage >= -12k){
-		additional_input->m_inf = 0.0k;
-
-	} else{
-	// Update m_inf (Substitute polynomial approximation)
-	additional_input->m_inf = 0.783385k + (-m_inf_V_shift) *
-							(1.42433k + (-m_inf_V_shift) *
-							(-3.00206k + (-m_inf_V_shift) *
-							(-3.70779k + (-m_inf_V_shift) *
-							(12.1412k + 15.3091k * (-m_inf_V_shift)))));
-	}
-	if (membrane_voltage <= -12k){
-		additional_input->e_to_t_on_tau_m_approx = 1.0k;
-
-	} else if (membrane_voltage >= 112k) {
-		additional_input->e_to_t_on_tau_m_approx = 0.0k;
-
-	} else {
-    // Update exp(t/tau_m) (Substitute polynomial approximation)
-	additional_input->e_to_t_on_tau_m_approx = 0.783385k + (-tau_m_V_shift) *
-							(1.42433k + (-tau_m_V_shift) *
-							(-3.00206k + (-tau_m_V_shift) *
-							(-3.70779k + (-tau_m_V_shift) *
-							(12.1412k + 15.3091k * (-tau_m_V_shift)))));
-	}
+	additional_input->e_to_t_on_tau_m_approx = expk(-0.1 *
+			((expk(-14.59 - 0.086 * membrane_voltage))
+			+ expk(-1.87 + 0.0701 * membrane_voltage))
+			);
 
 	// Update m
 	additional_input->m = additional_input->m_inf +
@@ -67,12 +40,13 @@ static input_t additional_input_get_input_value_as_current(
 			0.001k *
 			additional_input->m *
 			(membrane_voltage - -65k); //additional_input->E_H);
-//	log_info("mem_V: %k, m: %k, m_inf: %k, tau_m: %k, I_H = %k",
-//			membrane_voltage,
-//			additional_input->m,
-//			additional_input->m_inf,
-//			additional_input->e_to_t_on_tau_m_approx,
-//			additional_input->I_H);
+
+	log_info("mem_V: %k, m: %k, m_inf: %k, tau_m: %k, I_H = %k",
+			membrane_voltage,
+			additional_input->m,
+			additional_input->m_inf,
+			additional_input->e_to_t_on_tau_m_approx,
+			additional_input->I_H);
 
 //    return additional_input->I_H;
     return additional_input->I_H;
