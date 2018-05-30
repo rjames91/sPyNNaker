77220003d8483cf11054ec69bfbf358b36cb2ead
Mantas Mikaitis <mikaitis@cs.man.ac.uk>
2018-05-30 14:45:53 +0100

    Add solution for D'(t) in potassium current

--- neural_modelling/src/neuron/additional_inputs/additional_input_potassium.h
+++ neural_modelling/src/neuron/additional_inputs/additional_input_potassium.h
@@ -34,13 +34,11 @@ static input_t additional_input_get_input_value_as_current(
 
     additional_input->NaInflux = 0.025k / (1k + expk(-(membrane_voltage- -10k) * 0.2k)); //1/5 = 0.2
 
+    accum D_infinity = 1250k * additional_input->NaInflux + 0.001k;
 
-/////////////////////  NOT FIXED YET ////////////////
-	// Update D
-	additional_input->D = (additional_input->NaInflux * 0.025 + 0.0000008k) *   // Deq/tauD = 0.001/1250
-			(1k - additional_input->e_to_t_on_tau) +
-			additional_input->D * additional_input->e_to_t_on_tau;
-////////////////////////////////////////////////////
+	// Update D (Same form as LIF dV/dt solution)
+	additional_input->D = D_infinity +
+        (additional_input->D - D_infinity) * additional_input->e_to_t_on_tau;
 
 	additional_input->m_inf = 1k / (1k + (0.0078125k /                          // 0.25^3.5 = 0.0078125
                                           (additional_input->D
